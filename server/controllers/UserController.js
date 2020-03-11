5ab76940469bc4e09b26f3b957cc0d6b702a9fbf
agunggst <rqz.agastya@gmail.com>
2020-03-11 17:07:40 +0700

    controller done

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,10 +1,49 @@
 const { User } = require('../models')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 require('dotenv').config()
 
 class UserController {
     static login(request, response, next) {
-
+        let data_login = {
+            email: request.body.email,
+            password: request.body.password
+        }
+        let data_user
+        User.findOne({
+            where: {
+                email: data_login.email
+            }
+        })
+        .then( result => {
+            if(result){
+                data_user = result
+                return bcrypt.compare(data_login.password, result.password)
+            }else{
+                throw {
+                    status_code: 404,
+                    message: 'User Not Found'
+                }
+            }
+        } )
+        .then( result => {
+            if(result){
+                let token = jwt.sign({
+                    id: data_user.id,
+                    name: data_user.name,
+                    email: data_user.email
+                })
+                response.status(200).json(token)
+            }else{
+                throw {
+                    status_code: 400,
+                    message: 'Wrong Password'
+                }
+            }
+        } )
+        .catch( err => {
+            next(err)
+        } )
     }
 
     static register(request, response, next){
@@ -34,6 +73,7 @@ class UserController {
                 name: result.name,
                 email: result.email
             }, process.env.JWT_SECRET)
+            response.status(201).json({access_token: token})
         } )
         .catch( err => {
             next(err)
