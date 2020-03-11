4d18dcd63e970141555471b6a1cdbb9159450e36
agunggst <rqz.agastya@gmail.com>
2020-03-11 15:46:31 +0700

    sudah bisa masukin data ke card, dtanya masih dummy

--- client/script/main.js
+++ client/script/main.js
@@ -1,23 +1,68 @@
-// localStorage.setItem('access_token', 'abcd')
+localStorage.setItem('access_token', 'abcd')
 var app = new Vue({
     el: '#app',
     data: {
-        login_box: (localStorage.getItem('access_token')?false:true),
-        no_token: (localStorage.getItem('access_token')?false:true),
-        register_box: false,
-        user_info_box: (localStorage.getItem('access_token')?true:false),
-        kanban_box: (localStorage.getItem('access_token')?true:false),
-        yes_token: (localStorage.getItem('access_token')?true:false),
-        message: 'aaaaaaa'
+        name: 'John Doe',
+        task: [
+            {
+                title: 'Membuat Todo Backlog',
+                category: 'Backlog',
+                description: 'Mebuat SPA TodoApp buat tugas portfolio minggu 1 Backlog'
+            },
+            {
+                title: 'Membuat Todo Todo',
+                category: 'Todo',
+                description: 'Mebuat SPA TodoApp buat tugas portfolio minggu 1 Todo'
+            },
+            {
+                title: 'Membuat Todo Done',
+                category: 'Done',
+                description: 'Mebuat SPA TodoApp buat tugas portfolio minggu 1 Done'
+            },
+            {
+                title: 'Membuat Todo Completed',
+                category: 'Completed',
+                description: 'Mebuat SPA TodoApp buat tugas portfolio minggu 1 Completed'
+            },
+            {
+                title: 'Membuat Todo Backlog',
+                category: 'Backlog',
+                description: 'Mebuat SPA TodoApp buat tugas portfolio minggu 1 Backlog'
+            },
+            {
+                title: 'Membuat Todo Done',
+                category: 'Done',
+                description: 'Mebuat SPA TodoApp buat tugas portfolio minggu 1 Done'
+            }
+        ],
+        isLogin: (localStorage.getItem('access_token')?true:false),
+        regis_login: true
     },
     methods: {
-        showRegister: () => {
-            this.app.login_box = false
-            this.app.register_box = true
+        showRegister: function() {
+            console.log(this.isLogin)
+            this.regis_login = false
         },
-        showLogin:() => {
-            this.app.register_box = false
-            this.app.login_box = true
+        showLogin: function() {
+            this.regis_login = true
+        },
+        logout: function() {
+            localStorage.removeItem('access_token'),
+            this.isLogin = false
+        }
+    },
+    computed: {
+        backlog_content() {
+            return this.task.filter( (data) => data.category == 'Backlog' )
+        },
+        todo_content() {
+            return this.task.filter( (data) => data.category == 'Todo' )
+        },
+        done_content() {
+            return this.task.filter( (data) => data.category == 'Done' )
+        },
+        completed_content() {
+            return this.task.filter( (data) => data.category == 'Completed' )
         }
     }
 })
